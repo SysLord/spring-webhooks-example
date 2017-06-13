6e881a8567a21ed43d85077e5d492399bc07681f
Christian Helmer <lordhomepages@gmx.de>
2017-06-13 14:42:14 +0200

    subscriptionrepository improved for use as a base class

--- spring-webhooks/src/main/java/de/syslord/microservices/webhooks/subscription/SubscriptionRepository.java
+++ spring-webhooks/src/main/java/de/syslord/microservices/webhooks/subscription/SubscriptionRepository.java
@@ -1,23 +1,60 @@
 package de.syslord.microservices.webhooks.subscription;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
 
 import de.syslord.microservices.webhooks.events.SubscriptionEvent;
+import de.syslord.microservices.webhooks.utils.AsyncServiceCaller;
+import de.syslord.microservices.webhooks.utils.ServiceCall;
 
-public interface SubscriptionRepository {
+public abstract class SubscriptionRepository {
+
+	private static final Logger logger = LoggerFactory.getLogger(SubscriptionRepository.class);
+
+	@Autowired
+	private AsyncServiceCaller asyncServiceCaller;
 
 	@PreAuthorize("hasRole('ROLE_SUBSCRIBER') && @eventSecurity.hasEventPermission(#subscription.event)")
-	void add(String username, Subscription subscription) throws SubscriptionException;
+	public abstract void add(String username, Subscription subscription) throws SubscriptionException;
 
 	@PreAuthorize("hasRole('ROLE_SUBSCRIBER') && @eventSecurity.hasEventPermission(#subscription.event)")
-	void patch(String username, Subscription subscription) throws SubscriptionException;
+	public abstract void patch(String username, Subscription subscription) throws SubscriptionException;
 
 	@PreAuthorize("hasRole('ROLE_SUBSCRIBER')")
-	void delete(String username, String id);
+	public abstract void delete(String username, String id);
 
 	@PreAuthorize("hasRole('ROLE_SUBSCRIBER')")
-	UserSubscriptions getSubscriptionsForUser(String username);
+	public abstract UserSubscriptions getSubscriptionsForUser(String username);
 
-	void fireEvent(SubscriptionEvent subscriptionEvent);
+	protected abstract List<Subscription> getSubscriptionsForEvent(String eventname);
+
+	public void fireEvent(SubscriptionEvent subscriptionEvent) {
+		List<Subscription> callList = getSubscriptionsForEvent(subscriptionEvent.getName());
+
+		String owners = callList.stream()
+			.map(s -> s.getOwner())
+			.distinct()
+			.collect(Collectors.joining(", "));
+
+		logger.info("fire event {}: {} calls for {}", subscriptionEvent.getName(), callList.size(), owners);
+
+		callList.stream()
+			.map(subscription -> {
+				String address = subscriptionEvent.applyPlaceholder(subscription.getRemoteAdress());
+				String body = subscriptionEvent.applyPlaceholder(subscription.getPostBody());
+
+				ServiceCall serviceCall = new ServiceCall(
+						subscription.getHttpMethod(), address,
+						subscription.getUsername(), subscription.getPassword(),
+						body);
+				return serviceCall;
+			})
+			.forEach(serviceCall -> asyncServiceCaller.enqueue(serviceCall));
+	}
 
 }
